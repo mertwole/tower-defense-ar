df032e6f341a91447778caa877d3f39e8fdefb0d
mertwole <mertwole@gmail.com>
2024-11-18 14:26:27 +0400

    Support all fingers

--- Hands/HandInput.cs
+++ Hands/HandInput.cs
@@ -2,21 +2,32 @@ using Godot;
 
 public partial class HandInput : XRController3D
 {
+	enum PinchGesture
+	{
+		Index,
+		Middle,
+		Ring,
+		Little
+	}
+
 	[Export]
 	private ReadInput handInput;
 
 	private XRHandle activeHandle = null;
+	private PinchGesture? pinchGesture = null;
 
 	public override void _Process(double delta)
 	{
-		activeHandle?.Move(handInput.PinchPosition);
+		activeHandle?.Move(GetPinchPosition());
 	}
 
 	private void OnButtonPressed(string name)
 	{
-		if (name == "index_pinch")
+		pinchGesture = EventNameToPinchGesture(name);
+
+		if (pinchGesture != null)
 		{
-			var position = handInput.PinchPosition;
+			var position = GetPinchPosition();
 
 			activeHandle = TryFindHandle(position);
 			activeHandle?.Take(position);
@@ -25,11 +36,39 @@ public partial class HandInput : XRController3D
 
 	private void OnButtonReleased(string name)
 	{
-		if (name == "index_pinch")
+		var gesture = EventNameToPinchGesture(name);
+
+		if (gesture != null && gesture == pinchGesture)
 		{
 			activeHandle?.Release();
 			activeHandle = null;
 		}
+	}
+
+	private Vector3 GetPinchPosition()
+	{
+		var fingerPosition = pinchGesture switch
+		{
+			PinchGesture.Index => handInput.IndexTipPosition,
+			PinchGesture.Middle => handInput.MiddleTipPosition,
+			PinchGesture.Ring => handInput.RingTipPosition,
+			PinchGesture.Little => handInput.LittleTipPosition,
+			_ => handInput.ThumbTipPosition
+		};
+
+		return (handInput.ThumbTipPosition + fingerPosition) * new Vector3(.5f, .5f, .5f);
+	}
+
+	private PinchGesture? EventNameToPinchGesture(string name)
+	{
+		return name switch
+		{
+			"index_pinch" => PinchGesture.Index,
+			"middle_pinch" => PinchGesture.Middle,
+			"ring_pinch" => PinchGesture.Ring,
+			"little_pinch" => PinchGesture.Little,
+			_ => null
+		};
 	}
 
 	private XRHandle TryFindHandle(Vector3 position)
--- Hands/ReadInput.cs
+++ Hands/ReadInput.cs
@@ -3,28 +3,47 @@ using Godot;
 public partial class ReadInput : Node3D
 {
 	[Export]
+	private string thumbTipBoneName;
+	[Export]
 	private string indexTipBoneName;
 	[Export]
-	private string thumbTipBoneName;
+	private string middleTipBoneName;
+	[Export]
+	private string ringTipBoneName;
+	[Export]
+	private string littleTipBoneName;
 
 	Skeleton3D skeleton;
-	int indexFingerTipBone;
-	int thumbFingerTipBone;
 
-	public Vector3 IndexTipPosition { get; private set; }
+	int thumbFingerTipBone;
+	int indexFingerTipBone;
+	int middleFingerTipBone;
+	int ringFingerTipBone;
+	int littleFingerTipBone;
+
 	public Vector3 ThumbTipPosition { get; private set; }
-	public Vector3 PinchPosition { get => (IndexTipPosition + ThumbTipPosition) * new Vector3(.5f, .5f, .5f); }
+	public Vector3 IndexTipPosition { get; private set; }
+	public Vector3 MiddleTipPosition { get; private set; }
+	public Vector3 RingTipPosition { get; private set; }
+	public Vector3 LittleTipPosition { get; private set; }
 
 	public override void _Ready()
 	{
 		skeleton = FindChild("Skeleton3D", true) as Skeleton3D;
-		indexFingerTipBone = skeleton.FindBone(indexTipBoneName);
+
 		thumbFingerTipBone = skeleton.FindBone(thumbTipBoneName);
+		indexFingerTipBone = skeleton.FindBone(indexTipBoneName);
+		middleFingerTipBone = skeleton.FindBone(middleTipBoneName);
+		ringFingerTipBone = skeleton.FindBone(ringTipBoneName);
+		littleFingerTipBone = skeleton.FindBone(littleTipBoneName);
 	}
 
 	public override void _Process(double delta)
 	{
-		IndexTipPosition = skeleton.ToGlobal(skeleton.GetBoneGlobalPoseNoOverride(indexFingerTipBone).Origin);
 		ThumbTipPosition = skeleton.ToGlobal(skeleton.GetBoneGlobalPoseNoOverride(thumbFingerTipBone).Origin);
+		IndexTipPosition = skeleton.ToGlobal(skeleton.GetBoneGlobalPoseNoOverride(indexFingerTipBone).Origin);
+		MiddleTipPosition = skeleton.ToGlobal(skeleton.GetBoneGlobalPoseNoOverride(middleFingerTipBone).Origin);
+		RingTipPosition = skeleton.ToGlobal(skeleton.GetBoneGlobalPoseNoOverride(ringFingerTipBone).Origin);
+		LittleTipPosition = skeleton.ToGlobal(skeleton.GetBoneGlobalPoseNoOverride(littleFingerTipBone).Origin);
 	}
 }
